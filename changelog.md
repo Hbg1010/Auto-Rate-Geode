9e6ac1ef8a0b54e74913151656a81a0166e6a877
hbg1010 <harrisong26@parkschool.net>
2026-01-03 17:24:04 -0500

    Automatic Demon rating + auto rating downloaded levels

--- changelog.md
+++ changelog.md
@@ -1,2 +1,9 @@
+# 1.1.0
+- Added automatic demon ratings
+    - You can toggle Automatic demon ratings in this mod's settings
+    - You can automatically rate demons based on their current difficulty or input your own custom ratings
+- Fixed automatically rating downloaded levels
+- JITless iOS support (experimental)
+
 # 1.0.0
 - Auto rate was born
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,62 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/LevelInfoLayer.hpp>
 #include <random>
 
 using namespace geode::prelude;
 
-#include <Geode/modify/LevelInfoLayer.hpp>
+#define GET_SETTING_BOOL(mod, key) mod->getSettingValue<bool>(key)
+#define GET_SETTING_STRING(mod, key) mod->getSettingValue<std::string>(key)
+
 class $modify(AutoRateLayer, LevelInfoLayer) {
+    struct Fields {
+        bool hasChecked; // used to check if a level has been liked / evaluated already
+    };
+
     void levelDownloadFinished(GJGameLevel* p0) {
         LevelInfoLayer::levelDownloadFinished(p0);
+        AutoRate();
+    }
 
-        if (Mod::get()->getSettingValue<bool>("enable") && !p0->m_stars.value() && m_starRateBtn && m_starRateBtn->isEnabled()) {
-            RateStarsLayer* rsl = RateStarsLayer::create(m_level->m_levelID, p0->isPlatformer(), false);
-            int number = p0->m_starsRequested;
-            if (!number) {
-                int avg = p0->getAverageDifficulty();
-                number = avg > 0 ? avg : AutoRateLayer::randInt();
+    void updateSideButtons() {
+        if (!LevelInfoLayer::shouldDownloadLevel()) AutoRate();
+        LevelInfoLayer::updateSideButtons();
+
+    }
+
+    void AutoRate() {
+        Mod* modPointer = Mod::get();
+        if (GET_SETTING_BOOL(modPointer, "enable") && !m_fields->hasChecked) {
+            // unrated
+            if (!m_level->m_stars.value() && m_starRateBtn && m_starRateBtn->isEnabled()) {
+                RateStarsLayer* rsl = RateStarsLayer::create(m_level->m_levelID, m_level->isPlatformer(), false);
+                int starVal = m_level->m_starsRequested;
+                if (!starVal) {
+                    int avg = m_level->getAverageDifficulty();
+                    starVal = avg > 0 ? avg : AutoRateLayer::randInt();
+                }
+
+                rsl->m_starsRate = starVal;
+                rsl->onRate(rsl->m_submitButton);
+                CC_SAFE_DELETE(rsl);
+                auto spr = CCSprite::createWithSpriteFrameName("GJ_starBtn2_001.png");
+                m_starRateBtn->setSprite(spr);
+                m_starRateBtn->m_bEnabled = false;
+
+            // demon rates
+            } else if (m_level->m_demon.value() && GET_SETTING_BOOL(modPointer, "enableDemon") && m_demonRateBtn && m_demonRateBtn->isEnabled()) {
+                RateDemonLayer* RDL = RateDemonLayer::create(m_level->m_levelID);
+                std::string mode = GET_SETTING_STRING(modPointer, "DemonRatingMethod");
+                RDL->m_demonRate = mode == "Automatic" 
+                    ? DemonAutoVal(m_level->m_demonDifficulty)
+                    : DemonOverrideVal(mode);
+                RDL->onRate(RDL->m_submitButton);
+                CC_SAFE_DELETE(RDL);
+                auto spr = CCSprite::createWithSpriteFrameName("GJ_rateDiffBtn2_001.png");
+                m_demonRateBtn->setSprite(spr);
+                m_demonRateBtn->m_bEnabled = false;
             }
-
-            rsl->m_starsRate = number;
-            rsl->onRate(rsl->m_submitButton);
         }
+        m_fields->hasChecked = true;
     }
 
     // for those pesky "I didn't request a rating" guys (You know who you are) with 0 player submitted rating
@@ -27,12 +65,32 @@ class $modify(AutoRateLayer, LevelInfoLayer) {
         std::uniform_int_distribution<> distrib(1, 10);
         return distrib(gen);
     }
-};
 
-// #include <Geode/modify/RateStarsLayer.hpp>
-// class $modify(rs, RateStarsLayer) {
-//     void onRate(CCObject* sender) {
-//         RateStarsLayer::onRate(sender);
-//         log::debug("{}", m_starsRate);
-//     }
-// };
+    // i hate this mapping so much dude just make it make sense 
+    int DemonAutoVal (int difficulty) {
+        switch (difficulty)
+        {
+            case 3: return 1;
+            case 4: return 2;
+            case 5: return 4;
+            case 6: return 5;
+            case 0: return 3;
+            default: return 3;
+        }
+    }
+
+    // custom vals. I didn't wanna make a custom settings menu for an enum sry
+    int DemonOverrideVal(std::string& val) {
+        if (val == "Easy") {
+            return 1; 
+        } else if (val == "Medium") {
+            return 2;
+        } else if (val == "Hard") {
+            return 3;
+        } else if (val == "Insane") {
+            return 4;
+        } else {
+            return 5;
+        }
+    }
+};
